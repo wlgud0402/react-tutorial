caf8880dd0d194a870bca9bef6076e4e265db307
wlgud0402 <wlgudrlgus@naver.com>
2020-11-10 19:23:11 +0900

    bind and setState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: "welcome",
+      mode: "read",
       subject: { title: "WEB", sub: "World Wide Web!!" },
       welcome: { title: "Welcome", desc: "Hello, React!!" },
       contents: [
@@ -42,8 +42,12 @@ class App extends Component {
               onClick={function (e) {
                 console.log(e);
                 e.preventDefault(); //이벤트가 발생하구 페이지 리로드를 막아준다.
+                // this.state.mode = "welcome";
+                this.setState({
+                  mode: "welcome",
+                });
                 // debugger;
-              }}
+              }.bind(this)}
             >
               {this.state.subject.title}
             </a>
