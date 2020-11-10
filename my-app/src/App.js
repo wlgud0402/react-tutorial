93b904b7c914f13546e76ef27f1538bd4eac3471
wlgud0402 <wlgudrlgus@naver.com>
2020-11-10 19:18:02 +0900

    event default

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: "welcome",
       subject: { title: "WEB", sub: "World Wide Web!!" },
+      welcome: { title: "Welcome", desc: "Hello, React!!" },
       contents: [
         { id: 1, title: "HTML", desc: "HTML is for imformation" },
         { id: 2, title: "CSS", desc: "CSS is for design" },
@@ -17,15 +19,39 @@ class App extends Component {
     };
   }
   render() {
+    let _title,
+      _desc = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subject
+        {/* <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}
-        ></Subject>
-        <Subject title="React" sub="For UI"></Subject>
+        ></Subject> */}
+        {/* <Subject title="React" sub="For UI"></Subject> */}
+        <header>
+          <h1>
+            <a
+              href="/"
+              onClick={function (e) {
+                console.log(e);
+                e.preventDefault(); //이벤트가 발생하구 페이지 리로드를 막아준다.
+                // debugger;
+              }}
+            >
+              {this.state.subject.title}
+            </a>
+          </h1>
+          {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents}></TOC>
-        <Content data={this.state.contents}></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
