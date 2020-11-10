37e3c8c2d657a75b4ccb2a029fa7372402729008
wlgud0402 <wlgudrlgus@naver.com>
2020-11-10 19:05:19 +0900

    use key

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,16 +5,27 @@ import Subject from "./components/Subject";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "WEB", sub: "World Wide Web!!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is for imformation" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JavaScript", desc: "JavaScript is for interactivs" },
+      ],
+    };
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject>
         <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
-        <Content
-          title="HTML"
-          desc="HTML is HyperText Markup Language."
-        ></Content>
+        <TOC data={this.state.contents}></TOC>
+        <Content data={this.state.contents}></Content>
       </div>
     );
   }
--- my-app/src/components/TOC.jsx
+++ my-app/src/components/TOC.jsx
@@ -2,19 +2,20 @@ import React, { Component } from "react";
 
 class TOC extends Component {
   render() {
+    let lists = [];
+    let data = this.props.data;
+    let i = 0;
+    while (i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a href={"/content/" + data[i].id}>{data[i].title}</a>
+        </li>
+      );
+      i++;
+    }
     return (
       <nav>
-        <ul>
-          <li>
-            <a href="1.html">HTML</a>
-          </li>
-          <li>
-            <a href="2.html">CSS</a>
-          </li>
-          <li>
-            <a href="3.html">JavaScript</a>
-          </li>
-        </ul>
+        <ul>{lists}</ul>
       </nav>
     );
   }
